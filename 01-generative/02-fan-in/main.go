ac17a15895f2df91adcacae29fd3ece57dbc1c59
Vitaliy Likhachev <valikhachev@avito.ru>
2023-05-03 10:20:54 +0400

    minor fixes

--- 01-generative/02-fan-in/main.go
+++ 01-generative/02-fan-in/main.go
@@ -35,20 +35,21 @@ func producer(name string, done <-chan struct{}, wg *sync.WaitGroup) <-chan payl
 	return ch
 }
 
-func consumer(channels []<-chan payload, done <-chan struct{}, wg *sync.WaitGroup) {
+func consumer(channels []<-chan payload, done <-chan struct{}, wg *sync.WaitGroup, fanIn chan<- payload) {
 	for i, ch := range channels {
 		i := i + 1
 		ch := ch
 		go func() {
 			defer wg.Done()
-			fmt.Println("started consumer", i)
+			fmt.Println("started consume of producer", i)
 			for {
 				select {
 				case <-done:
-					fmt.Println("consumer", i, "completed")
+					fmt.Println("consume of producer", i, "completed")
 					return
 				case v := <-ch:
-					fmt.Println("Consumer", i, "got value", v.value, "from", v.name)
+					fmt.Println("consumer of producer", i, "got value", v.value, "from", v.name)
+					fanIn <- v
 				}
 			}
 		}()
@@ -64,8 +65,21 @@ func main() {
 	producers = append(producers, producer("Alice", done, &wg))
 	producers = append(producers, producer("Jack", done, &wg))
 
+	fanIn := make(chan payload, 0)
+
 	wg.Add(2)
-	consumer(producers, done, &wg)
+	consumer(producers, done, &wg, fanIn)
+
+	go func() {
+		for {
+			select {
+			case <-done:
+				return
+			case v := <-fanIn:
+				fmt.Printf("fanIn got %v\n", v)
+			}
+		}
+	}()
 
 	time.Sleep(time.Second)
 	close(done)
